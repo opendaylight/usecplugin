0da770694814a07bf0498361046f85145e153476
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-09-21 20:25:44 -0400

    Depend on Beryllium dependencies
    Change-Id: If612ba47e799084d66fb77057239c375f331ab47
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- impl/src/main/java/org/opendaylight/usecplugin/impl/UsecpluginProvider.java
+++ impl/src/main/java/org/opendaylight/usecplugin/impl/UsecpluginProvider.java
@@ -1,11 +1,13 @@
 /*
- * Tata Consultancy Services and others.  All rights reserved.
+ * Copyright (c) 2015 Tata Consultancy Services and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.usecplugin.impl;
+
 import org.opendaylight.controller.sal.binding.api.NotificationService;
 import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
