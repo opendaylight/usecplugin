7ff7e993b1b8adf1daaae6c22ff428bdc57fdf8e
thomaslees <thomaslee.s@tcs.com>
2015-11-10 22:40:13 +0530

    Implementation of RPCs and Notification as well as SQLite DB creation.
    Change-Id: I9f518fb21e72a0094e7577128a19490391556cb1
    Signed-off-by: thomaslees <thomaslee.s@tcs.com>

---
+++ impl/src/main/java/org/opendaylight/usecplugin/impl/UsecpluginNotifImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Tata Consultancy Services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.usecplugin.impl;
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.UsecpluginService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.UsecpluginListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.LowWaterMarkBreached;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.LowWaterMarkBreachedBuilder;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.NotificationListener;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+
+
+public class UsecpluginNotifImpl implements UsecpluginListener {
+	
+	@Override
+	public void onLowWaterMarkBreached(LowWaterMarkBreached notification) {
+		
+    System.out.println(" Inside UsecpluginNotifImpl ");
+	      
+			
+		}
+		
+        
+    }
+	
+	
+	
+
+	
+	
+	
+
--- impl/src/main/java/org/opendaylight/usecplugin/impl/UsecpluginProvider.java
+++ impl/src/main/java/org/opendaylight/usecplugin/impl/UsecpluginProvider.java
@@ -5,36 +5,72 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.usecplugin.impl;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.sal.binding.api.NotificationService;
+
 import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.UsecpluginService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UsecpluginProvider implements BindingAwareProvider, AutoCloseable{
+
+
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.NotificationListener;
+import org.opendaylight.yangtools.yang.binding.Notification;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.UsecpluginListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.LowWaterMarkBreached;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usecplugin.rev150105.LowWaterMarkBreachedBuilder;
+
+
+public class UsecpluginProvider implements  BindingAwareProvider, AutoCloseable{
 
     private static final Logger LOG = LoggerFactory.getLogger(UsecpluginProvider.class);
+    private RpcRegistration<UsecpluginService> usecpluginService;
+   
+    LowWaterMarkBreached notification;
+    
     private DataBroker dataBroker;
-
+    
     @Override
     public void onSessionInitiated(ProviderContext session) {
-        LOG.info("UsecpluginProvider Session Initiated");
+    	
+    	LOG.info("UsecpluginProvider Session Initiated");
+    	
+        System.out.println("UsecpluginProvider Session Initiated");
+        
+        usecpluginService = session.addRpcImplementation(UsecpluginService.class, new UsecpluginRPCImpl());
+       
         NotificationService notificationService = session.getSALService(NotificationService.class);
         PacketHandler packetHandler = new PacketHandler();
-        Registration packetInRegistration = notificationService.registerNotificationListener(packetHandler);
+      
+        notificationService.registerNotificationListener(packetHandler);
         dataBroker = session.getSALService(DataBroker.class);
         packetHandler.setdataBroker(dataBroker);
-
+        packetHandler.dbOpen();
+        
+        
+            UsecpluginNotifImpl listener = new UsecpluginNotifImpl();
+               
+			listener.onLowWaterMarkBreached(notification);
+               
+		
+       
     }
 
     @Override
     public void close() throws Exception {
         LOG.info("UsecpluginProvider Closed");
+        if (usecpluginService != null) {
+        	usecpluginService.close();
+        }
+       
+        
     }
 }
