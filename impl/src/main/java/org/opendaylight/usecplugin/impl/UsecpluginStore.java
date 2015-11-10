df70c2cf7de3a95978b0b6cd581c8060f780bb43
ThomasLee <thomaslee.s@tcs.com>
2015-11-10 15:36:37 +0000

    Implementation of RPCs and Notification as well as SQLite DB creation.
    Change-Id:I9f518fb21e72a0094e7577128a19490391556cb1
    Signed-off-by: thomaslees <thomaslee.s@tcs.com>

--- impl/src/main/java/org/opendaylight/usecplugin/impl/UsecpluginStore.java
+++ impl/src/main/java/org/opendaylight/usecplugin/impl/UsecpluginStore.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015 Tata Consultancy Services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.usecplugin.impl;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -15,9 +23,6 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by user on 9/9/15.
- */
 public class UsecpluginStore {
     private static final Logger LOG = LoggerFactory.getLogger(UsecpluginStore.class);
     private DataBroker dataBroker;
